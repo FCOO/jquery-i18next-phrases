71768621d82e431ab24767b8963bb4461130775b
Niels Holt <niels@steenbuchholt.dk>
2017-03-10 10:43:44 +0100

    First version

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # jquery-i18next-phrases
-jQuery-fn-methods to add translations using i18next-phrases
+[i18next]:http://i18next.com
+[i18next-phrases]:https://github.com/FCOO/i18next-phrases
+[jquery-i18next]:https://github.com/i18next/jquery-i18next
+
+
+## Description
+jQuery-fn-methods to add [i18next] translations using [jquery-i18next] and [i18next-phrases]
+
+Add new methods to jQuery prototype: 
+
+    $.fn.i18n( htmlOrKeyOrPhrase[, attribute][, options] )
+
+Add/updates the `"data-i18n"` attribute
+
+`htmlOrKeyOrPhrase` = 
+
+- a simple html-string - `"This will <u>always</u> be in English"` OR 
+- a [i18next]-key - `"myNS:myKey"` OR 
+- a [i18next-phrases]-object - `{da:"Dette er en test", en:"This is a test"}` OR
+- a string representing a [i18next-phrases]-object - `"{'da':'Dette er en test', 'en':'This is a test'}"`
+
+
+
+## Installation
+### bower
+`bower install https://github.com/FCOO/jquery-i18next-phrases.git --save`
+
+## Demo
+http://FCOO.github.io/jquery-i18next-phrases/demo/ 
+
+## Usage
+
+    $('selector').i18n("This will <u>always</u> be in English"); //Simple (html) string
+
+    $('selector2').i18n("myNamespace:myKey"); //[namespace]-key added to i18next
+
+    $('selector3').i18n({da:"Dette er en test", en:"This is a test"}); //As i18next-phrase
+    
+    $('selector4').i18n({da:"Dette er en titel", en:"This is a title"}, 'title'); //With attribute
+
+    $('selector5').i18n("{'da':'Dette er en anden test', 'en':'This is another test'}"); //i18next-phrase as string
+
+
+## Copyright and License
+This plugin is licensed under the [MIT license](https://github.com/FCOO/jquery-i18next-phrases/LICENSE).
+
+Copyright (c) 2017 [FCOO](https://github.com/FCOO)
+
+## Contact information
+
+Niels Holt nho@fcoo.dk
---
+++ dist/jquery-i18next-phrases.js
@@ -0,0 +1,126 @@
+/****************************************************************************
+	jquery-i18next-phrases.js, 
+
+	(c) 2017, FCOO
+
+	https://github.com/FCOO/jquery-i18next-phrases
+	https://github.com/FCOO
+
+****************************************************************************/
+
+(function ($, window/*, document, undefined*/) {
+	"use strict";
+	
+    /***********************************************************
+    Initialize jquery-i18next - i18next plugin for jquery 
+    https://github.com/i18next/jquery-i18next
+    ***********************************************************/
+    var jQuery_i18n_selectorAttr = 'data-i18n',    // selector for translating elements
+        jQuery_i18n_targetAttr   = 'i18n-target',  // data-() attribute to grab target element to translate (if diffrent then itself)
+        jQuery_i18n_optionsAttr  = 'i18n-options'; // data-() attribute that contains options, will load/set if useOptionsAttr = true
+
+    
+    window.jqueryI18next.init(
+        window.i18next/*i18nextInstance*/, 
+        $, 
+        {
+            tName         : 't',                        // --> appends $.t = i18next.t
+            i18nName      : 'i18n',                     // --> appends $.i18n = i18next
+            handleName    : 'localize',                 // --> appends $(selector).localize(opts);
+            selectorAttr  : jQuery_i18n_selectorAttr,   // selector for translating elements
+            targetAttr    : jQuery_i18n_targetAttr,     // data-() attribute to grab target element to translate (if diffrent then itself)
+            optionsAttr   : jQuery_i18n_optionsAttr,    // data-() attribute that contains options, will load/set if useOptionsAttr = true
+            useOptionsAttr: true,                       // see optionsAttr
+            parseDefaultValueFromContent: true          // parses default values from content ele.val or ele.text
+        }
+    );
+
+
+    /***********************************************************
+    Add new methods to jQuery prototype: 
+    $.fn.i18n( htmlOrKeyOrPhrase[, attribute][, options] )
+    Add/updates the "data-i18n" attribute
+
+    htmlOrKeyOrPhrase = simple html-string ( "This will <u>always</u> be in English" ) OR 
+                        i18next-key ( "myNS:myKey" ) OR 
+                        a phrase-object - see langValue in i18next.addPhrase ( {da:"Dette er en test", en:"This is a test"} ) OR
+                        a string representing a phrase-object ( '{"da":"Dette er en test", "en":"This is a test"}' )
+    ***********************************************************/
+    var tempKeyId = 0,
+        tempNS = '__TEMP__';
+
+    $.fn.i18n = function( htmlOrKeyOrPhrase ) {
+        var options = null, 
+            attribute = '', 
+            argument,
+            isKey = true,
+            key = htmlOrKeyOrPhrase;
+
+        for (var i=1; i<arguments.length; i++ ){
+            argument = arguments[i];              
+            switch ($.type(argument)){
+              case 'object': options = argument; break;
+              case 'string': attribute = argument; break;
+            }
+        }
+
+        var original = htmlOrKeyOrPhrase;
+        try {
+            htmlOrKeyOrPhrase = JSON.parse(htmlOrKeyOrPhrase);
+        }
+        catch (e) {
+            htmlOrKeyOrPhrase = original; 
+        }
+
+        //Get the key or add a temp-phrase
+        if (typeof htmlOrKeyOrPhrase == 'string')//{
+            isKey = !!(window.i18next.t( htmlOrKeyOrPhrase, {defaultValue:''} ));
+        else {
+            //It is a {da:'...', en:'...', de:'...'} object
+            key = 'jqueryfni18n' + tempKeyId++;
+            window.i18next.addPhrase( tempNS, key, htmlOrKeyOrPhrase );
+            key = tempNS+':'+key;
+        }    
+
+        if (isKey)
+            return this.each(function() {
+                var $this = $(this),
+                    oldData = $this.attr( jQuery_i18n_selectorAttr ),
+                    newData = [],
+                    oldStr,
+                    newStr = attribute ? '[' + attribute + ']' + key : key,
+                    keep;
+                oldData = oldData ? oldData.split(';') : [];
+            
+                for (var i=0; i<oldData.length; i++ ){
+                    oldStr = oldData[i];
+                    keep = true;
+                    //if the new key has an attribute => remove data with '[attribute]'
+                    if (attribute && (oldStr.indexOf('[' + attribute + ']') == 0))
+                        keep = false;                      
+                    //if the new key don't has a attribute => only keep other attributes
+                    if (!attribute && (oldStr.indexOf('[') == -1)) 
+                        keep = false;
+                    if (keep)
+                        newData.push( oldStr );
+                }
+                newData.push( newStr);                                
+
+                //Set data-i18n
+                $this.attr( jQuery_i18n_selectorAttr, newData.join(';') );
+
+                //Set data-i18n-options
+                if (options)
+                    $this.attr( 'data-' + jQuery_i18n_optionsAttr, JSON.stringify( options ) );
+
+                //Update contents
+                $this.localize();        
+            });
+        else
+            //Not a key => simple add htmlOrKeyOrPhrase as html
+            return this.each(function() {
+                $(this).html( htmlOrKeyOrPhrase );
+            });
+    };
+
+}(jQuery, this, document));
